90a5a35b439fdaa6e35c0b4ca2a94558f06dc7b2
indratamagiri <madegiri1212@gmail.com>
2018-11-30 15:00:03 +0700

    Updates

---
+++ precache-manifest.be1be87b9542c03698468daeec45de6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "546420916fbb18a00c9e",
+    "url": "/contactmanager/static/js/main.54642091.chunk.js"
+  },
+  {
+    "revision": "dc282341161d9bbb1c64",
+    "url": "/contactmanager/static/js/1.dc282341.chunk.js"
+  },
+  {
+    "revision": "546420916fbb18a00c9e",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "dc282341161d9bbb1c64",
+    "url": "/contactmanager/static/css/1.64fe8f45.chunk.css"
+  },
+  {
+    "revision": "f036d013cf6284459fd315fc7555849f",
+    "url": "/contactmanager/index.html"
+  }
+];
